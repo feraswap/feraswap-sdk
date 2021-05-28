1480878fd0c1a65fd15db2ca1c119b50e6c50f09
ali ak <alisgumush@gmail.com>
2021-05-28 12:44:23 +0300

    update

--- src/entities/token.ts
+++ src/entities/token.ts
@@ -79,5 +79,5 @@ export const WETH = {
     'Wrapped Ether'
   ),
   [ChainId.GÖRLI]: new Token(ChainId.GÖRLI, '0xB4FBF271143F4FBf7B91A5ded31805e42b2208d6', 18, 'WETH', 'Wrapped Ether'),
-  [ChainId.KOVAN]: new Token(ChainId.KOVAN, '0x812A2eeed6b4A9eA86CA4f5915ff546292E5F039', 18, 'WETH', 'Wrapped Ether')
+  [ChainId.KOVAN]: new Token(ChainId.KOVAN, '0x58759c10AeC1CD1104929E3024B6D4CC63b323F5', 18, 'WETH', 'Wrapped Ether')
 }
